7647c33f3b69fe1b9327f26ad7880a48cfaa25ed
Nick Garbalau <garbalaunick@protonmail.com>
2022-04-18 15:07:20 +0300

    Update graphql package

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -13,15 +13,9 @@ const Navbar = () => {
       <Head>
         <title>Earthquiet</title>
         <link rel='preconnect' href='https://fonts.googleapis.com' />
-        <link rel='preconnect' href='https://fonts.gstatic.com' crossorigin />
+        <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin />
         <link
           href='https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;500&display=swap'
-          rel='stylesheet'
-        />
-        <link rel='preconnect' href='https://fonts.googleapis.com' />
-        <link rel='preconnect' href='https://fonts.gstatic.com' crossorigin />
-        <link
-          href='https://fonts.googleapis.com/css2?family=Lato:wght@100;300;400;700&family=Roboto:wght@100;300;500&display=swap'
           rel='stylesheet'
         />
       </Head>
