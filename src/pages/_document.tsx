a7889196c0aaf31130d42693c90ba0f547113fc5
Nick Garbalau <garbalaunick@protonmail.com>
2022-05-27 17:18:32 +0300

    Remove script tag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,11 +5,6 @@ class DocumentPage extends Document {
     return (
       <Html>
         <Head>
-          <script
-            async
-            src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8031403896285102'
-            crossOrigin='anonymous'
-          ></script>
           <link
             href='https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500&display=swap'
             rel='stylesheet'
